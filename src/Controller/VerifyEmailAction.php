9584edf1d6630754e1d5ef78a031ae3eba098121
knarficious <knarficious@free.fr>
2023-09-09 13:19:25 +0200

    user operation

--- src/Controller/VerifyEmailAction.php
+++ src/Controller/VerifyEmailAction.php
@@ -30,11 +30,11 @@ final class VerifyEmailAction extends AbstractController
         //$this->emailVerifier = $emailVerifier;
     }
     
-    public function __invoke(Request $request, UserRepository $userRepository): User
+    public function __invoke(Request $request, User $user): User
     {
-        $userId = $request->get('userId');        
+//         $userId = $request->get('userId');        
 
-        $user = $userRepository->findOneBy(['username' => $userId]);        
+//         $user = $userRepository->findOneBy(['username' => $userId]);        
 
         // validate email confirmation link, sets User::isVerified=true and persists
         try {
