29a38af6ddc00042607167031d6a5d6cb11ec0d2
knarficious <knarficious@free.fr>
2023-09-09 13:06:51 +0200

    user operation

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -27,7 +27,7 @@ use App\Controller\RegisterAction;
 #[ApiResource(
 operations: [
     new Get(uriTemplate: '/users/{id}'),
-    //new Get(name: '_api_/verify', routeName: 'app_verify_email'),
+    new Get(name: '_api_/verify', routeName: 'app_verify_email'),
     new GetCollection(uriTemplate: '/users'),
     new Post(uriTemplate: 'users', controller: RegisterAction::class),    
 #new Post(processor: UserPasswordHasher::class, validationContext: ['groups' => ['Default', 'user:create']]),
