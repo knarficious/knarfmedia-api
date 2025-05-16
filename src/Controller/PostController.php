62cebb578dc52982f85bde1d9e0a654044f4d47b
knarficious <franckruer@orange.fr>
2025-05-16 11:46:20 +0200

    refactor controller

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -14,22 +14,10 @@ final class PostController extends AbstractController
 {
     public function __invoke(Request $request, EntityManagerInterface $em): Publication
     {
-//         $data = json_decode($request->getContent(), true);
-        
-//         $post = new Publication();
-        
-//         $post->setTitle($data['title']);
-//         $post->setSummary($data['summary']);
-//         $post->setAuthor($this->getUser());
-//         $post->setContent($data['content']);
-//         $post->setPublishedAt(new \DateTime());
-        
-//         return $post;
-
-        $post = $request->attributes->get('data');
-        if (!($post instanceof Publication))
+        $publication = $request->attributes->get('data');
+        if (!($publication instanceof Publication))
         {
-            throw new \RuntimeException('L\'objet n\'est pas une instance de Post');
+            throw new \RuntimeException('L\'objet n\'est pas une instance de Publication');
         }
         
         $uploadedFile = $request->files->get('file');
@@ -37,12 +25,12 @@ final class PostController extends AbstractController
 //             throw new BadRequestHttpException('"file" is required');
 //         }
         
-        $post->setFile($uploadedFile);
+        $publication->setFile($uploadedFile);
         //$post->setUpdatedAt(new \DateTime()); 
-        $post->setAuthor($this->getUser());
-        $post->setTitle($request->attributes->get('data')->getTitle());
-        $post->setSummary($request->attributes->get('data')->getSummary());
-        $post->setContent($request->attributes->get('data')->getContent());
+        $publication->setAuthor($this->getUser());
+        $publication->setTitle($request->attributes->get('data')->getTitle());
+        $publication->setSummary($request->attributes->get('data')->getSummary());
+        $publication->setContent($request->attributes->get('data')->getContent());
         $tags = $request->attributes->get('data')->getTags();
             
             foreach ($tags as $tag){
@@ -50,15 +38,10 @@ final class PostController extends AbstractController
                 $tagClass = $em->getReference($tagReference, $tag->getId());
                 
                 if ($em->contains($tagClass)) {
-                    $post->addTag($tagClass);
+                    $publication->addTag($tagClass);
                 }
-               
-
-               }             
-                
-            
+               }
         
-        
-        return $post;
+        return $publication;
     }
 }
